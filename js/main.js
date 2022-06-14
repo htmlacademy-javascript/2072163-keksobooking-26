5f8d1d6f5ba364100f39c7379d2b26f06b8713bc
Kirill Dubovoy <josevecancari@gmail.com>
2022-06-14 07:17:47 +0400

    внесение констант

--- js/main.js
+++ js/main.js
@@ -1,28 +1,27 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно
+const ERROR_MESSAGE = 'Задан неверный параметр! Укажите другие данные.';
+
+const validateArgs = function (min, max) {
+  return !(typeof min !== 'number' || typeof max !== 'number' || min < 0 || max < 0);
+};
+
+const getRandomNumber = function (min, max) {
+  return Math.random() * (max - min + 1) + min;
+};
+
+const makeValid = function (min, max) {
+  return max < min ? [min, max] = [max, min] : [min, max];
+};
 const getRandomInteger = function (min, max) {
-  //Использование деструктуризации
-  if (max < min) {
-    [min, max] = [max, min];
-  }
-  if (min < 0 || max < 0) {
-    return 'Задан неверный параметр! Укажите другие данные.';
-  }
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  if (!validateArgs(min, max)) {return ERROR_MESSAGE;}
+  return Math.floor(getRandomNumber(...makeValid(min, max)));
 };
+getRandomInteger(1, 10);
 
-getRandomInteger(4, 9);
 
-//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-const getRandomFloat = function (min, max, signPoint) {
-  //Использование деструктуризации
-  if (max < min) {
-    [min, max] = [max, min];
-  }
-  if (min < 0 || max < 0) {
-    return 'Задан неверный параметр! Укажите другие данные.';
-  }
-  return (Math.random() * (max - min + 1) + min).toFixed(signPoint);
+const getRandomFloat = function(min, max, signPoint) {
+  if (!validateArgs(min, max)) {return ERROR_MESSAGE;}
+  return getRandomNumber(...makeValid(min, max)).toFixed(signPoint);
 };
+getRandomFloat (1, 2, 3);
 
 
-getRandomFloat(1, 3, 2);
---
+++ js/more_details.js
@@ -0,0 +1,12 @@
+const TITLE = ['Заголовок 1', 'Заголовок 2', 'заголвок 3', 'Заголовок 4'];
+const TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const CHECKIN = ['12:00', '13:00', '14:00'];
+const CHECKOUT = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner', ];
+const DESCRIPTION = ['Описание 1', 'Описание 2', 'Описание 3', 'Описание 4', ];
+const PHOTOS = [
+  https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg,
+  https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg,
+  https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg,
+
+];
